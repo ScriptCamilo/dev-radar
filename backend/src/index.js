a46a17e683418d47f8bdf7c9d05c140d433e8dbe
Azelve <azelve@protonmail.ch>
2020-04-07 14:21:27 -0300

    Application finished

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http')
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app)
+
+setupWebsocket(server)
+
+mongoose.connect('mongodb+srv://camilo:amoregina@cluster0-omxpq.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes)
+
+
+app.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (indetificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+
+
